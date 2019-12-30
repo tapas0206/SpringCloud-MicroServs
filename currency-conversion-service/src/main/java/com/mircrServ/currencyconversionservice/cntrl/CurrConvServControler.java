c5a06e9b52cfab086c85253b11f6a2f9661d998e
tapas0206 <tapas.agrawal55@gmail.com>
2019-12-30 17:40:42 +0400

    Curr-Conv-Serv-Setp1

--- currency-conversion-service/src/main/java/com/mircrServ/currencyconversionservice/cntrl/CurrConvServControler.java
+++ currency-conversion-service/src/main/java/com/mircrServ/currencyconversionservice/cntrl/CurrConvServControler.java
@@ -1,10 +1,15 @@
 package com.mircrServ.currencyconversionservice.cntrl;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.mircrServ.currencyconversionservice.dto.CurrConversionDTO;
 
@@ -15,11 +20,21 @@ public class CurrConvServControler {
 	private org.springframework.core.env.Environment environment;
 	
 
-	@GetMapping("/CurrConverter")
-	public CurrConversionDTO ConverionService() {
-//		CurrConversionDTO conversionDTO=new CurrConversionDTO();
-//		conversionDTO.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
-		return new CurrConversionDTO((long) 100,"USD","INR",BigDecimal.valueOf(10),BigDecimal.valueOf(650),
-				Integer.parseInt(environment.getProperty("local.server.port")),BigDecimal.valueOf(65));
+	@GetMapping("/CurrConverter/from/{from}/to/{to}/quanitiy/{quanitiy}")
+	public CurrConversionDTO ConverionService(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quanitiy) {
+		
+		Map<String,String> uriVariable=new HashMap<>();
+		uriVariable.put("from", from);
+		uriVariable.put("to", to);
+		
+		
+		ResponseEntity<CurrConversionDTO> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/Curr-Excg-Serv/from/{from}/to/{to}", CurrConversionDTO.class,uriVariable);
+		CurrConversionDTO response = responseEntity.getBody();
+		
+		
+		CurrConversionDTO conversionDTO=new CurrConversionDTO(response.getId(),from,to,quanitiy,quanitiy.multiply(response.getConversionVal()),
+				Integer.parseInt(environment.getProperty("local.server.port")),response.getConversionVal());
+		
+		return conversionDTO;
 	}
 }
