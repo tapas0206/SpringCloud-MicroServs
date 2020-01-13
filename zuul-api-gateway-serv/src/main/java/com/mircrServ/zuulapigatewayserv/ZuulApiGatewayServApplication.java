01c400bc66adfde2f9d06c8003133c38046b65d1
tapas0206 <tapas.agrawal55@gmail.com>
2020-01-13 15:14:55 +0400

    Zuul Added 02

---
+++ zuul-api-gateway-serv/src/main/java/com/mircrServ/zuulapigatewayserv/ZuulApiGatewayServApplication.java
@@ -0,0 +1,13 @@
+package com.mircrServ.zuulapigatewayserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiGatewayServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServApplication.class, args);
+	}
+
+}
---
+++ zuul-api-gateway-serv/src/test/java/com/mircrServ/zuulapigatewayserv/ZuulApiGatewayServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircrServ.zuulapigatewayserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
